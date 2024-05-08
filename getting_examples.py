545cfed10e1d52f755887b980d798620f0d2a546
sammiphone6 <sammit@mit.edu>
2024-05-08 00:18:31 -0400

    minor edits

--- getting_examples.py
+++ getting_examples.py
@@ -9,7 +9,7 @@ def fetch_and_parse_json(url):
     else:
         raise Exception(f"Failed to fetch data: {response.status_code}")
 
-def get_activation_data(url):
+def get_activation_data_for_feature(url):
     """
     STRUCTURE OF cleaned_data
 
@@ -42,7 +42,7 @@ def get_activation_data(url):
     return cleaned_data
 
 url = "https://www.neuronpedia.org/api/feature/gpt2-small/9-res-jb/0"
-data = get_activation_data(url)
+data = get_activation_data_for_feature(url)
 
 pprint.pprint(data)
 print(len(data['examples'])) ## Has 76 examples
