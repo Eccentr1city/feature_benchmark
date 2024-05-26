f4af29cc6c834ca3b44882ea8101cc4456c99f24
root <adam_kaufman@college.harvard.edu>
2024-05-26 07:24:17 +0000

    analyzing recomputed values

--- reVals_vs_val.py
+++ reVals_vs_val.py
@@ -1,32 +1,60 @@
 import json
-import matplotlib.pyplot as plt
+import os
+import numpy as np
 
-# Load the JSON data from the file
-with open('6-res-jb_subset_100/428.json', 'r') as file:
-    data = json.load(file)
+def recomputed_change(activation):
+    eps = 1e-8
+    values = activation['values']
+    recomputed = activation['recomputedValues'][1:]
+    elem_change = [((re - val) /  (max(val, re) + eps)) for re, val in zip(recomputed, values)]
+    if (max(sum(values), sum(recomputed))) == 0:
+        mag_ratio = 0
+    else:
+        mag_ratio = sum(recomputed) / (max(sum(values), sum(recomputed)))
+    return elem_change, mag_ratio
 
-# Initialize lists to store the IDs, recomputed values, and original values
-data_by_id = []
 
-# Loop through each entry in the posActivations
-for activation in data['posActivations']:
-    entry = {
-        'id': activation['id'],
-        'recomputed_values': activation['recomputedValues'][1:],  # Remove the first element from recomputed values
-        'values': activation['values']
-    }
-    data_by_id.append(entry)
+def compare_recomputed_values(file_path):
+    # Load the JSON data from the file
+    with open(file_path, 'r') as file:
+        data = json.load(file)
 
-# Print each id with its corresponding recomputed values and values
+    # (recomputed - value) / value
+    pos_elementwise_percent_change = []
+    neg_elementwise_percent_change = []
+    # sum(recomputed) / max(sum(values), sum(recomputed))
+    pos_magnitude_ratios = []
+    neg_magnitude_ratios = []
 
-for item in data_by_id:
-    plt.figure(figsize=(10, 5))
-    plt.plot(item['values'], label='Values', marker='o')
-    plt.plot(item['recomputed_values'], label='Recomputed Values', marker='x')
-    plt.title(f"Graph for ID: {item['id']}")
-    plt.xlabel('Index')
-    plt.ylabel('Value')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # Loop through each entry in the posActivations
+    for activation in data['posActivations']:
+        elem_change, mag_ratio = recomputed_change(activation)
+        pos_elementwise_percent_change.append(elem_change)
+        pos_magnitude_ratios.append(mag_ratio)
+    for activation in data['negSelfActivations']:
+        elem_change, mag_ratio = recomputed_change(activation)
+        neg_elementwise_percent_change.append(elem_change)
+        neg_magnitude_ratios.append(mag_ratio)
 
+    return pos_elementwise_percent_change, neg_elementwise_percent_change, pos_magnitude_ratios, neg_magnitude_ratios
+
+
+def compare_recomputed_group(directory):
+    file_names = []
+    mean_pos_ratio = []
+    mean_neg_ratio = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            file_path = os.path.join(directory, filename)
+        file_names.append(os.path.basename(file_path))
+        pos_elementwise_percent_change, neg_elementwise_percent_change, pos_magnitude_ratios, neg_magnitude_ratios = compare_recomputed_values(file_path)
+        if len(pos_magnitude_ratios) > 0:
+            mean_pos_ratio.append(np.array(pos_magnitude_ratios).mean())
+        else:
+            mean_pos_ratio.append(0)
+        if len(neg_magnitude_ratios) > 0:
+            mean_neg_ratio.append(np.array(neg_magnitude_ratios).mean())
+        else:
+            mean_neg_ratio.append(0)
+
+    return file_names, mean_pos_ratio, mean_neg_ratio
